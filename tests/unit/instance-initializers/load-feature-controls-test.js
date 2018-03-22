de6ddd32b4f4377b8452bc303c6673ffd7882ae0
Hugo Vieilledent <hugo.vieilledent@people-doc.com>
2018-03-22 18:49:13 +0100

    feature: instance initializer to set feature-flag fron local-storage

---
+++ tests/unit/instance-initializers/load-feature-controls-test.js
@@ -0,0 +1,11 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Instance Initializer | load-feature-controls', function(hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test('it works', async function(assert) {
+    assert.ok(true);
+  });
+});
