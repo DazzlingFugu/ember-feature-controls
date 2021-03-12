6205181ac6d3b0e557f86c263253934dc004724d
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2021-03-12 12:38:21 +0000

    chore: update lint config of HBS files

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,8 +1,54 @@
 'use strict'
 
 module.exports = {
-  extends: 'octane',
+  extends: [
+    'octane',
+    'stylistic',
+  ],
+
   rules: {
-    'no-action': 0
-  }
+    'attribute-indentation': {
+      'indentation': 2,
+      'open-invocation-max-len': 100,
+    },
+
+    'inline-link-to': 'error',
+
+    'no-bare-strings': 'error',
+
+    'no-class-bindings': 'error',
+
+    'no-element-event-actions': 'error',
+
+    'no-link-to-tagname': 'error',
+
+    'no-model-argument-in-route-templates': 'error',
+
+    'no-this-in-template-only-components': 'error',
+
+    'no-yield-to-default': 'error',
+
+    'require-form-method': 'warn',
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/tests/dummy/**/*.hbs',
+        '**/tests/**/*.{js,ts}',
+      ],
+      rules: {
+        'no-bare-strings': 'off',
+      },
+    },
+    {
+      files: [
+        '**/tests/dummy/app/templates/**/*.hbs',
+      ],
+
+      rules: {
+        'no-this-in-template-only-components': 'off',
+      },
+    },
+  ],
 }
