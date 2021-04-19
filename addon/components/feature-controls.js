55d314244cd2adbe9b176b3b6475de760fff0586
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2021-04-19 11:30:33 +0100

    refactor: refactor code and improve readability

--- addon/components/feature-controls.js
+++ addon/components/feature-controls.js
@@ -39,25 +39,28 @@ export default class FeatureControls extends Component {
   @action
   refresh() {
     // Take the existing flags from the config and put them in a list of default values
-    let featureFlags = this.featureFlags
+    let { featureFlags } = this
     let defaults = {}
+
     for (let key in featureFlags) {
       defaults[this._normalizeFlag(key)] = featureFlags[key]
     }
+
     // Model is a local copy of the list of flags register for features service, used to compute properties on the full list
-    let model = (this.features.flags || []).map(key => {
-      let meta =
-        ((featureControls && this.featureControls.metadata) || []).find(
-          obj => {
-            return this._normalizeFlag(obj.key) === key
-          }
-        ) || {}
+    let model = (this.features.flags || []).map((key) => {
+      let meta = ((featureControls && this.featureControls.metadata) || []).find(
+        (obj) => this._normalizeFlag(obj.key) === key
+      ) || {}
+
       if (meta.hide === true) {
         return undefined
       }
-      let isFlagLS =
-        this.featureControls.useLocalStorage &&
-        this.get(`featureControlStorage.featuresLS.${key}`) !== undefined
+
+      let isFlagLS = (
+        this.featureControls.useLocalStorage
+        && this.get(`featureControlStorage.${key}`) !== undefined
+      )
+
       let featureFlag = {
         key,
         isEnabled: isFlagLS
@@ -65,18 +68,22 @@ export default class FeatureControls extends Component {
           : this.features.isEnabled(key),
         default: defaults[key] || false
       }
+
       return assign({}, meta, featureFlag)
     })
+
     set(this, "model", model.filter(item => item !== undefined))
   }
 
   @action
   reset() {
     // Reset the flags from the features service to the default value in the config
-    let featureFlags = this.featureFlags
-    Object.keys(featureFlags).forEach(key => {
+    let { featureFlags } = this
+
+    Object.keys(featureFlags).forEach((key) => {
       this.updateFeature(this._normalizeFlag(key), featureFlags[key])
     })
+
     // If we use local storage then we want to clear the stored data
     if (this.featureControls.useLocalStorage) {
       this.featureControlStorage.featuresLS.reset()
@@ -89,14 +96,15 @@ export default class FeatureControls extends Component {
     } else {
       this.features.disable(key)
     }
+
     // Update the local model accordingly
-    let model = this.model
-    let modelFlag = model.find(obj => {
-      return obj.key === key
-    })
+    let { model } = this
+    let modelFlag = model.find((obj) => obj.key === key)
+
     if (modelFlag) {
       set(modelFlag, "isEnabled", isEnabled)
       set(this, "model", model)
+
       if (modelFlag.reload) {
         windowUtil.reload()
       }
@@ -106,6 +114,7 @@ export default class FeatureControls extends Component {
   @action
   doToggleFeature(key, checkboxState) {
     this.updateFeature(key, !checkboxState)
+
     if (this.featureControls.useLocalStorage) {
       this.set(`featureControlStorage.featuresLS.${key}`, !checkboxState)
     }
