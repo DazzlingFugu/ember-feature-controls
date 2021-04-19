24a28ad53e556d9d017b542eca6212a56de22d1b
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2021-04-19 11:44:10 +0100

    chore: disable lint rule

--- addon/components/feature-controls.js
+++ addon/components/feature-controls.js
@@ -1,3 +1,4 @@
+/* eslint-disable ember/no-classic-components */
 import Component from "@ember/component"
 import layout from "../templates/components/feature-controls"
 import { inject as service } from "@ember/service"
